ce5ab743a0493f0d517055800a10499e305ad8a0
GeariousStudios <gearious.studios@gmail.com>
2025-05-22 21:57:16 +0200

    H

--- backend/obj/Debug/net8.0/eCommerce.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/eCommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eCommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4a9915c70905f53b3cf8f637a38850473042828")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc9aae449d9d3dce21d8af2b7612ca2a9262ed34")]
 [assembly: System.Reflection.AssemblyProductAttribute("eCommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eCommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
