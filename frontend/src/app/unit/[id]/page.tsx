4a503d0ac9b8f15da357b66b0fd82905251dc43d
GeariousStudios <gearious.studios@gmail.com>
2025-06-22 20:36:29 +0200

    Fix

--- frontend/src/app/unit/[id]/page.tsx
+++ frontend/src/app/unit/[id]/page.tsx
@@ -5,23 +5,24 @@ export default function Page() {
   return <UnitWrapper />;
 }
 
-export async function generateStaticParams(): Promise<{ id: string }[]> {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-  const response = await fetch(`${apiUrl}/unit`, {
-    headers: { "Content-Type": "application/json" },
-  });
+// export async function generateStaticParams(): Promise<{ id: string }[]> {
+//   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  
+//   const response = await fetch(`${apiUrl}/unit`, {
+//     headers: { "Content-Type": "application/json" },
+//   });
 
-  if (!response.ok) {
-    notFound();
-  }
+//   if (!response.ok) {
+//     notFound();
+//   }
 
-  const data = await response.json();
+//   const data = await response.json();
 
-  if (!data?.items) {
-    notFound();
-  }
+//   if (!data?.items) {
+//     notFound();
+//   }
 
-  return data.items.map((unit: any) => ({
-    id: unit.id.toString(),
-  }));
-}
+//   return data.items.map((unit: any) => ({
+//     id: unit.id.toString(),
+//   }));
+// }
